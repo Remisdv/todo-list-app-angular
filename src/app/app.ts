9b3e0b4987c8f7269b0b32c123dfa1ca7c74c478
RemiSDV <lhuillierremi1@gmail.com>
2025-12-03 11:12:48 +0100

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
-import { HeaderTitle } from './header-title/header-title';
 import { TodoItem } from './todo-item/todo-item';
 import { Task } from './todo-item/model/Task';
 import { TodoService } from './services/todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderTitle, TodoItem, AsyncPipe],
+  imports: [RouterOutlet, TodoItem, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
