ea573729902e37e8d4e82ab7460c1c481ff828b9
RemiSDV <lhuillierremi1@gmail.com>
2025-12-04 15:08:31 +0100

    fix

--- src/app/feature/Task/component/task-detail/task-detail-page.ts
+++ src/app/feature/Task/component/task-detail/task-detail-page.ts
@@ -2,27 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { TodoService } from '../../service/todo';
 import { Task } from '../../../../shared/model/Task';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-task-detail-page',
-  imports: [AsyncPipe, JsonPipe, RouterLink],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './task-detail-page.html',
   styleUrl: './task-detail-page.css',
 })
-export class TaskDetailPage implements OnInit {
-  task$!: Observable<Task | undefined>;
+export class TaskDetailPage {
 
-  constructor(
-    private route: ActivatedRoute,
-    private todoService: TodoService
-  ) {}
-
-  ngOnInit() {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    if (id) {
-      this.task$ = this.todoService.getById(id);
-    }
-  }
 }
